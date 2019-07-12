c2a3befb6920cb7dc5cea12f5f494d50d595fb35
Ilya Moskalev <ilya.moskalev@oracle.com>
2019-07-13 00:04:39 +0200

    added integration tests

---
+++ Integration/TestContext.cs
@@ -0,0 +1,34 @@
+using AccountOwnerServer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+
+public class TestContext : IDisposable
+{
+  private TestServer _server;
+  public HttpClient Client { get; private set; }
+
+  public TestContext()
+  {
+    SetUpClient();
+  }
+
+  private void SetUpClient()
+  {
+    _server = new TestServer(new WebHostBuilder()
+      .UseConfiguration(new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .Build())
+      .UseStartup<Startup>());
+
+    Client = _server.CreateClient();
+  }
+
+  public void Dispose()
+  {
+    _server?.Dispose();
+    Client?.Dispose();
+  }
+}
